a54f497fefb2c12dfa48ae2eb83789e2e23a51e2
Alex330790 <ajkul@mail.ru>
2024-06-03 23:59:33 +0300

    ignore and modification

--- src/test/java/geo/GeoServiceTest.java
+++ src/test/java/geo/GeoServiceTest.java
@@ -10,60 +10,44 @@ import ru.netology.entity.Location;
 import ru.netology.geo.GeoService;
 import ru.netology.geo.GeoServiceImpl;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class GeoServiceTest<assertEquals> {
     @ParameterizedTest
-    @CsvSource(value = {"127.0.0.1,     null,       null,   null,       0",
+    @CsvSource(value = {"127.0.0.1,,,,       0",
             "172.0.32.11,   Moscow,     RUSSIA, Lenina,     15",
-            "96.44.183.149, 'New York', USA,    10th Avenue,32",
-            "172.111.111.111,Moscow,    RUSSIA, null,       0",
-            "96.111.111.111,'New York', USA,    null,       0"})
-    void testLocationIP(String ipRes, String city, String country, String street, int building) {
-//    void testLocation(String ipRes, String city){
+            "96.44.183.149, 'New York', USA,   ' 10th Avenue',32",
+            "172.111.111.111,Moscow,    RUSSIA,,       0",
+            "96.111.111.111,'New York', USA,,       0"})
+    void testLocationIP(String ipRes, String city, Country country, String street, int building) {
+
         GeoService geoService = new GeoServiceImpl();
         Location locationOriginal = geoService.byIp(ipRes);
-
-
-//        LocalizationService localizationService = new LocalizationServiceImpl();
-        if (city.equals("null")) {
-            city = null;
-        }
-        if (street.equals("null")) {
-            street = null;
-        }
-        if (country.equals("null")) {
-            country = null;
-            locationOriginal = new Location(city, null, street, building);
-        } else {
-            locationOriginal = new Location(city, Country.valueOf(country), street, building);
-        }
-
         Assertions.assertEquals(city, locationOriginal.getCity());
-        if (country != null) {
-            Assertions.assertEquals(Country.valueOf(country), locationOriginal.getCountry());
-        } else {
-            Assertions.assertEquals(null, locationOriginal.getCountry());
-        }
+        Assertions.assertEquals(country, locationOriginal.getCountry());
         Assertions.assertEquals(street, locationOriginal.getStreet());
         Assertions.assertEquals(building, locationOriginal.getBuiling());
 
+
     }
     @Test
-    public void testCoordinate(){
-//      T.к. assertTrue не удалось задействовать, то сравниваю через Equals
+    public void testCoordinate() throws RuntimeException {
+
         double latitude = 330902;
         double longitude = 309390;
         GeoService geoService = new GeoServiceImpl();
         String msg;
-        RuntimeException runtimeException = null;
+        Throwable  runtimeException = null;
 
-        try {
-            geoService.byCoordinates(latitude, longitude);
-        } catch (RuntimeException e) {
-            runtimeException=e;
-        }
-
-
+        runtimeException = assertThrows(
+                Throwable.class,
+                () -> {
+                    geoService.byCoordinates(latitude, longitude);
+                }
+        );
         Assertions.assertEquals(RuntimeException.class, runtimeException.getClass());
+
+
     }
 
 
